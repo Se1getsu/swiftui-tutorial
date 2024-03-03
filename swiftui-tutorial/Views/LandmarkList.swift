6a27f3f87b65b1ff456117c5d7abcbbeb758fc4b
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2024-03-03 21:19:45 +0900

    Section 2: Building lists and navigation

---
+++ swiftui-tutorial/Views/LandmarkList.swift
@@ -0,0 +1,30 @@
+//
+//  LandmarkList.swift
+//  swiftui-tutorial
+//
+//  Created by Seigetsu on 2024/03/03
+//
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationSplitView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
